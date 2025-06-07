416bb2ec92e596125743ec36060c26a1ce2fb0e6
Thithira-Paranawithana <153026117+Thithira-Paranawithana@users.noreply.github.com>
2025-06-07 18:54:29 +0530

    configurations and new services

---
+++ api-gateway/src/test/java/com/dotsline/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotsline.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
